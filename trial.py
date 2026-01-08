1a1c279ea343db800444c9ba9efb3a86103d164a
Saniya Pathan <pathansaniya0103@gmail.com>
2026-01-08 15:25:33 +0530

    Updated logic for upload

--- trial.py
+++ trial.py
@@ -1,30 +1,20 @@
-import paho.mqtt.client as mqtt
-import json
-import time
+from Adafruit_IO import MQTTClient
 
-MQTT_BROKER = "398e8a90820d4da69bf78b8ace2a81b4.s1.eu.hivemq.cloud"
-MQTT_PORT = 8883
-MQTT_TOPIC = "test/pressure"
+ADAFRUIT_IO_USERNAME = "your_username"
+ADAFRUIT_IO_KEY = "your_aio_key"  # Get from Adafruit IO settings
+FEED_ID = "brake_pressure"
 
-# Use callback_api_version=1 to fix ValueError
-client = mqtt.Client(client_id="pi_test", callback_api_version=1)
-client.username_pw_set("Saniya_p", "Saniya@9786")
-client.tls_set()  # TLS for port 8883
+def connected(client):
+    print("Connected to Adafruit IO")
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected successfully!")
-    else:
-        print("Connection failed, code:", rc)
+def disconnected(client):
+    print("Disconnected from Adafruit IO")
 
-client.on_connect = on_connect
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.connect()
+client.loop_background()
 
-client.connect(MQTT_BROKER, MQTT_PORT)
-client.loop_start()
-
-# Publish some test messages
-for i in range(3):
-    data = {"pressure_value": i}
-    client.publish(MQTT_TOPIC, json.dumps(data))
-    print("Sent:", data)
-    time.sleep(2)
+# Send data
+client.publish(FEED_ID, 5.4)
